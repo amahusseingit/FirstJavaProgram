622f4425531a22cf41e4bfa41e043ea8bf33c584
Ali Hussein <amahussein@gmail.com>
2025-08-28 15:15:29 +0300

    commit a change from vs source countrol

--- java-project/src/Main.java
+++ java-project/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from a git branch");
+        System.out.println("Hello, World! ");
     }
 }
